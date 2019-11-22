0710d6784bee518cf59c54e654b0f6a489c455ed
Kevin De Magalhaes <nopseudo+git@gmail.com>
2019-11-23 00:27:37 +0100

    Updated script

--- scripts/ps/change-propreties-elements/chpropinf.ps1
+++ scripts/ps/change-propreties-elements/chpropinf.ps1
@@ -2,32 +2,51 @@
 #Get filelist in Path, recursivly, only .elmt extension then stock theses paths in $files array
 $files = @(Get-ChildItem -Path "elements" -Recurse -Include "*.elmt" | ForEach-Object { Write-Output $_.FullName })
 
+#Define texts
+$txtInternalCode = "Entrez le code interne de l'élément / ref. commune à tous les fabricants (Optionnel):"
+$txtManufacturer = "Entrez le nom du fabricant à inserer dans les éléments à modifier. (Defaut: Generic)"
+$txtDesignation = "Entrez la référence fabricant de l'élément:"
+$txtComment = "Entrez le commentaire de l'élément: (Optionnel)"
+$txtDescription = "Entrez la description textuelle de l'élément:"
+
 #Define author informations 
-#tag "informations" need to exist
 $authorInfo = @"
 Author: Kevin De Magalhaes (KDM)
 Github: https://github.com/kedema
 Year: 2019 
 "@
 
-if(($manufacturerSel = Read-Host "Entrez le fabricant à inserer dans les éléments à modifier. (Defaut: Generic)") -eq ''){$global:manufacturer = "Generic"}else{$global:manufacturer = "$manufacturerSel"}
 Write-Host -ForegroundColor Cyan "Mention to add/change: `r`n$authorInfo"
 
+#Config for libraries sort by manufacturer
+if(($manufacturerNew = Read-Host $txtManufacturer) -eq ''){ $script:manufacturer = "Generic" }else{ $script:manufacturer = $manufacturerNew }
 
 #Foreach File in Files array
-#Open, change information and save it
+#Open, change informations and save it
 # /!\ May have some encoding issues in powershell >4.0/5.0, not tested for now
 foreach ($file in $files) {
-    Write-Host -ForegroundColor Cyan "Working in file: $file"
-    $xdoc = ( Select-Xml -Path $file -XPath / ).Node
-    $internalCode =  $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='machine-manufacturer-reference']").InnerText
-    if (($internalCodeNew = Read-Host -Prompt "Entrez le code interne de l'élément/ref. commune à tous les constructeurs: (valeur actuelle: $internalCode)") -eq -not '') {
-        $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='machine-manufacturer-reference']").InnerText = $internalCodeNew }
-    $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='manufacturer']").InnerText = $manufacturer
-    $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='designation']").InnerText = Read-Host -Prompt "Entrez la référence fabricant de l'élément:"
-    $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='comment']").InnerText = Read-Host -Prompt "Entrez le commentaire de l'élément:"
-    $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='description']").InnerText = Read-Host -Prompt "Entrez la description textuelle de l'élément:"
-    $xdoc.Save($file)
-    Write-Host -ForegroundColor Green "File $file saved. Next..."
+    try {
+        Write-Host -ForegroundColor Cyan "Working in file: $file"
+        $xdoc = ( Select-Xml -Path $file -XPath / ).Node
+        $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='manufacturer']").InnerText = $manufacturer
+        $xdoc.SelectSingleNode("/definition/informations").InnerText = $authorInfo
+        $internalCode =  $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='machine-manufacturer-reference']").InnerText
+        $internalCodeNew = Read-Host -Prompt "$txtInternalCode [Actual: $internalCode ]"
+            if (-Not ($internalCodeNew -eq '')) { $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='machine-manufacturer-reference']").InnerText = $internalCodeNew }
+        $designation = $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='designation']").InnerText
+        $designationNew = Read-Host -Prompt "$txtDesignation [Actual: $designation ]"
+            if (-Not ($designationNew -eq '')) { $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='designation']").InnerText = $designationNew }
+        $comment = $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='comment']").InnerText
+        $commentNew = Read-Host -Prompt "$txtComment [Actual: $comment ]"
+            if (-Not ($commentNew -eq '')) { $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='comment']").InnerText = $commentNew }
+        $description = $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='description']").InnerText
+        $descriptionNew = Read-Host -Prompt "$txtDescription [Actual: $description ]"
+            if (-Not ($descriptionNew -eq '')) { $xdoc.SelectSingleNode("/definition/elementInformations/elementInformation[@name='description']").InnerText = $descriptionNew }
+        $xdoc.Save($file)
+        Write-Host -ForegroundColor Green "File $file saved. Next..."
+    } catch {
+        Write-Host -ForegroundColor Red "Error with file, verify if tag elementInformation exist or read only file."
+        Write-Warning -Message "$($_.Exception.Message)"
+    }
 }
 Write-Host -ForegroundColor Green "All Dones."
